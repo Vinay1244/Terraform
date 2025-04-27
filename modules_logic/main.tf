a54420c0b929e14e9d203e97153bfcc496d1fb0f
Vinay1244 <vinaydev.comp@gmail.com>
2025-04-27 23:42:54 +0530

    Update main.tf with github source

--- modules_logic/main.tf
+++ modules_logic/main.tf
@@ -4,6 +4,7 @@ region = "var.region_value"
 
 module "aws_instance" {
 source = "./modules_instance"
+#source = "git::https://github.com/Vinay1244/Terraform.git//modules_logic/modules_instance"
 ami_value = "ami-0c1907b6d738188e5"
 subnet_value="subnet-0f95624d3ff01aa62"
 instance_value="t2.micro"
